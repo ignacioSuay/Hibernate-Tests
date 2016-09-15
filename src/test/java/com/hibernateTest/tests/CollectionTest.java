6ee4709040b8117d5a02656d0b3636c5f708616a
Ignacio Suay <ignacio.suay@gmail.com>
2016-09-15 17:35:13 +0100

    ConcurrentHashMap vs Hashtable

--- src/test/java/com/hibernateTest/tests/CollectionTest.java
+++ src/test/java/com/hibernateTest/tests/CollectionTest.java
@@ -48,7 +48,8 @@ import java.util.*;
  *
  *  * EnumMap: for use with EnumKeys.
  *  * WeakHashMap is an implementation of the Map interface that stores only weak references to its keys. Storing only weak references allows a key-value pair to be garbage-collected when its key is no longer referenced outside of the WeakHashMap.
- *  *  ConcurrentHashMap: Threadsafe hash map.
+ *  *  ConcurrentHashMap: Threadsafe hash map. Get in not sunc so it is faster.
+ *  * HashTable: Is a threadsafe hash map too but the performamce is worse because it is syncronice all operations including get.
  */
 public class CollectionTest {
 
